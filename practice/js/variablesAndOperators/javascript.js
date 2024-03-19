0c6571599aedd3b87d0d0207cddc364a1f3c396f
Johny Xaxa <johnyxaxa@pm.me>
2024-03-18 23:53:04 -0500

    Declare, multiline declare and chnage of variable values using let keyword

--- practice/js/variablesAndOperators/javascript.js
+++ practice/js/variablesAndOperators/javascript.js
@@ -1,4 +1,15 @@
-let message;
-message ='Wakeup!';
+// let message;
+// message ='Wakeup!';
 
-alert (message)
+// alert (message)
+
+// let alertMessage = 'Wake up!';
+// alert (alertMessage)
+// declaring variale and assigning data to it in a single line;
+
+// let alertMessage ='Hello!';
+
+// alertMessage ='Bye!';
+
+// console.log(alertMessage)
+//Changing the values of the variables
